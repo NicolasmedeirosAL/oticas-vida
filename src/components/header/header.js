e5059effa29005e5ba694ea619b7938612e5b942
Nicolas Medeiros <nick.medeiros.758660@gmail.com>
2023-08-17 14:22:26 -0300

    alteração na capa e em alguns estilos

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,10 +1,13 @@
+import React from 'react'
 import './style.css'
+
+
 
 export default function Header() {
   return (
     <header id="topo">
       <div className="barra-de-navegacao limitar-secao">
-        <img src="./assets/logo.png" alt="imagem" />
+        <img src='/assets/logo.png' alt="imagem" />
 
         <nav className="opcoes-do-menu">
           <a href="#produtos">Produtos</a>
